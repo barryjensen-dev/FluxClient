25c241d278e0d870301e65d8254f83895ffe3dca
Barry Jensen <79224859+barryjensen-dev@users.noreply.github.com>
2025-05-10 16:06:14 -0500

    Update flux-client.user.js

--- flux-client.user.js
+++ flux-client.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Flux Client
 // @namespace    none
-// @version      1
+// @version      1.0.0
 // @description  A lightweight, customizable Tampermonkey script designed to enhance the user experience in browser-based applications or games. It provides modular functionality with user-controlled toggles for key features such as aimbot, ESP, custom crosshair, and anti-AFK. Each feature is integrated through keyboard shortcuts for fast activation, and the script maintains a clean interface while offering power-user utilities.
 // @author       barryjensen-dev
 // @match        *://*/*
